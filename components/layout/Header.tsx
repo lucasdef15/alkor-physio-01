cbbec425d1688563e56a35bace7b9edc4ffb219e
lucasdef15 <lucasdef15@hotmail.com>
2026-07-14 22:48:59 -0300

    feat: update Header component layout and logo details for improved design

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,23 @@
+import Link from 'next/link';
+
 import LogoSVG from '@/components/svg/LogoSVG';
 
 export default function Header() {
   return (
     <div className="fixed top-2 flex w-full place-content-center">
-      <header className="w-full max-w-[90%] rounded-[.8rem] border-2 border-zinc-500/50 p-2">
+      <header className="flex w-full max-w-[90%] justify-between rounded-[.8rem] border-2 border-zinc-500/50 p-2">
         <div className="flex items-center">
-          <LogoSVG className="aspect-auto h-15 w-auto" name="ΛLKOR" surname="LABS" />
+          <LogoSVG className="aspect-auto h-12 w-auto md:h-15" name="ΛLKOR" surname="PHYSIO" />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <button>whatsapp</button>
         </div>
       </header>
     </div>
